3ed0a368075539932d60372910115134d74553b2
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-05-06 20:31:44 +0200

    add task

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
 var http = require('http');
 var fs = require('fs');
-
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        
-        response.write('<h1>Hello World!</h1>');
-            response.end();
-    } else {
-            response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-            response.end();
-    }
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', function(err, data) {
+        	response.write(data);
+        	return response.end();
+        });
+    }	else {
+    	response.setHeader("Content-Type", "image/jpg; charset=utf-8");
+        response.statusCode = 404;
+            fs.readFile('./404.jpg', function(err, data) {
+            	response.write(data);
+        		return response.end();
+            });                        
+    	}
 });
-
 server.listen(8080);
